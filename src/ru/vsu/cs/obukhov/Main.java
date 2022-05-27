3763468759f772bd0bcb27d34f49b959532c3c6b
AL1ATE <102225423+AL1ATE@users.noreply.github.com>
2022-05-27 11:50:35 +0300

    Create Main.java

---
+++ src/ru/vsu/cs/obukhov/Main.java
@@ -0,0 +1,19 @@
+package ru.vsu.cs.obukhov;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int x = scan
+
+    }
+
+
+
+
+}
